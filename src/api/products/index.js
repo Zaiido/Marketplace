2050d50ae0607254c6948c25d8bd138343b0ecd9
Zaide Kurti <zaiido1920@gmail.com>
2023-03-03 13:11:27 +0100

    Reviews CRUD

--- src/api/products/index.js
+++ src/api/products/index.js
@@ -11,7 +11,12 @@ const productsRouter = Express.Router()
 productsRouter.get("/", async (request, response, next) => {
     try {
         const products = await getProducts()
-        response.send(products)
+        if (request.query && request.query.category) {
+            const productsPerCategory = products.filter(product => product.category === request.query.category)
+            response.send(productsPerCategory)
+        } else {
+            response.send(products)
+        }
     } catch (error) {
 
     }
@@ -62,7 +67,7 @@ productsRouter.put("/:productId", async (request, response, next) => {
             response.send(updatedProduct)
 
         } else {
-            next(createHttpError(400, { message: `Product with id ${request.params.productId} does not exist!` }))
+            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
         }
 
     } catch (error) {
@@ -77,6 +82,7 @@ productsRouter.delete("/:productId", async (request, response, next) => {
         const filteredProducts = products.filter(product => product._id !== request.params.productId)
 
         if (products.length !== filteredProducts.length) {
+            await writeProducts(filteredProducts)
             response.status(204).send()
         } else {
             next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
@@ -129,6 +135,90 @@ productsRouter.post("/:productId/reviews", checkReviewSchema, generateBadRequest
 
         response.status(201).send({ _id: newReview._id })
 
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+productsRouter.get("/:productId/reviews", async (request, response, next) => {
+    try {
+        const reviews = await getReviews()
+        const validProductId = reviews.some(review => review.productId === request.params.productId)
+        if (validProductId) {
+            const productReviews = reviews.filter(review => review.productId === request.params.productId)
+            response.send(productReviews)
+        } else {
+            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+productsRouter.get("/:productId/reviews/:reviewId", async (request, response, next) => {
+    try {
+        const reviews = await getReviews()
+        const validProductId = reviews.some(review => review.productId === request.params.productId)
+        if (validProductId) {
+            const foundReview = reviews.find(review => review._id === request.params.reviewId)
+            if (foundReview) {
+                response.send(foundReview)
+            } else {
+                next(createHttpError(404, { message: `Review with id ${request.params.reviewId} does not exist!` }))
+            }
+        } else {
+            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
+        }
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+productsRouter.put("/:productId/reviews/:reviewId", async (request, response, next) => {
+    try {
+        const reviews = await getReviews()
+        const validProductId = reviews.some(review => review.productId === request.params.productId)
+        if (validProductId) {
+            const index = reviews.findIndex(review => review._id === request.params.reviewId)
+            const oldReview = reviews[index]
+            const updatedReview = { ...oldReview, ...request.body, updatedAt: new Date() }
+            reviews[index] = updatedReview
+
+            await writeReviews(reviews)
+
+            response.send(updatedReview)
+        } else {
+            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
+        }
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+productsRouter.delete("/:productId/reviews/:reviewId", async (request, response, next) => {
+    try {
+        const reviews = await getReviews()
+        const validProductId = reviews.some(review => review.productId === request.params.productId)
+        if (validProductId) {
+            const filteredReviews = reviews.filter(review => review._id !== request.params.reviewId)
+
+            if (reviews.length !== filteredReviews.length) {
+                await writeReviews(filteredReviews)
+                response.status(204).send()
+            } else {
+                next(createHttpError(404, { message: `Review with id ${request.params.reviewId} does not exist!` }))
+            }
+
+        } else {
+            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
+        }
 
     } catch (error) {
         next(error)
