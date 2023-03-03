a8a27a40e3b3ed2fd73e36b8e48679b93d06de67
Zaide Kurti <zaiido1920@gmail.com>
2023-03-03 11:43:53 +0100

    Product image upload

--- src/api/products/index.js
+++ src/api/products/index.js
@@ -1,8 +1,10 @@
-import Express, { response } from 'express'
-import { getProducts, writeProducts } from '../../lib/fs-tools.js'
+import Express from 'express'
+import { getProducts, saveProductImage, writeProducts } from '../../lib/fs-tools.js'
 import { checkProductSchema, generateBadRequest } from './validators.js'
 import uniqid from 'uniqid'
 import createHttpError from 'http-errors'
+import multer from 'multer'
+import { extname } from 'path'
 
 const productsRouter = Express.Router()
 
@@ -84,6 +86,33 @@ productsRouter.delete("/:productId", async (request, response, next) => {
     }
 })
 
+
+productsRouter.post("/:productId/upload", multer().single("product"), async (request, response, next) => {
+    try {
+        const fileExtension = extname(request.file.originalname)
+        const fileName = request.params.productId + fileExtension
+        console.log(request.file)
+        await saveProductImage(fileName, request.file.buffer)
+
+        const products = await getProducts()
+        const index = products.findIndex(product => product._id === request.params.productId)
+        if (index !== -1) {
+            const oldProduct = products[index]
+            const updatedProduct = { ...oldProduct, imageUrl: `http://localhost:3000/images/products/${fileName}` }
+            products[index] = updatedProduct
+
+            await writeProducts(products)
+            response.send({ message: "Image uploaded" })
+
+        } else {
+            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
+        }
+
+
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 export default productsRouter
--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -2,12 +2,14 @@ import fs from 'fs-extra'
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
 
-const { readJSON, writeJSON } = fs
+const { readJSON, writeJSON, writeFile } = fs
 
 const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data")
 
 const productsJSONPath = join(dataFolderPath, "products.json")
+const productsImagesFolderPath = join(process.cwd(), "./public/images/products")
 
 
 export const getProducts = () => readJSON(productsJSONPath)
 export const writeProducts = (productsArray) => writeJSON(productsJSONPath, productsArray)
+export const saveProductImage = (fileName, fileContent) => writeFile(join(productsImagesFolderPath, fileName), fileContent)
