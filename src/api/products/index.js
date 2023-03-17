f42efd9e54af7cf4507fe7de60767f87bd341f10
Zaide Kurti <zaiido1920@gmail.com>
2023-03-17 13:09:31 +0100

    Products & Reviews CRUD

--- src/api/products/index.js
+++ src/api/products/index.js
@@ -1,35 +1,25 @@
 import Express from 'express'
-import { getProducts, getReviews, saveProductImage, writeProducts, writeReviews } from '../../lib/fs-tools.js'
-import { checkProductSchema, checkReviewSchema, generateBadRequest } from './validators.js'
-import uniqid from 'uniqid'
 import createHttpError from 'http-errors'
-import multer from 'multer'
-import { extname } from 'path'
+import { ProductsModel, ReviewsModel } from "./model.js"
 
 const productsRouter = Express.Router()
 
 productsRouter.get("/", async (request, response, next) => {
     try {
-        const products = await getProducts()
-        if (request.query && request.query.category) {
-            const productsPerCategory = products.filter(product => product.category === request.query.category)
-            response.send(productsPerCategory)
-        } else {
-            response.send(products)
-        }
-    } catch (error) {
+        const products = await ProductsModel.find()
+        response.send(products)
 
+    } catch (error) {
+        next(error)
     }
 })
-productsRouter.post("/", checkProductSchema, generateBadRequest, async (request, response, next) => {
+
+
+productsRouter.post("/", async (request, response, next) => {
     try {
-        const newProduct = { _id: uniqid(), ...request.body, createdAt: new Date(), updatedAt: new Date() }
-
-        const products = await getProducts()
-        products.push(newProduct)
-        await writeProducts(products)
-
-        response.status(201).send({ _id: newProduct._id })
+        const newProduct = new ProductsModel(request.body)
+        const { _id } = await newProduct.save()
+        response.status(201).send({ _id })
 
     } catch (error) {
         next(error)
@@ -40,13 +30,9 @@ productsRouter.post("/", checkProductSchema, generateBadRequest, async (request,
 
 productsRouter.get("/:productId", async (request, response, next) => {
     try {
-        const products = await getProducts()
-        const foundProduct = products.find(product => product._id === request.params.productId)
-        if (foundProduct) {
-            response.send(foundProduct)
-        } else {
-            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
-        }
+        const foundProduct = await ProductsModel.findById(request.params.productId)
+        if (!foundProduct) return next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
+        response.send(foundProduct)
     } catch (error) {
         next(error)
     }
@@ -55,21 +41,13 @@ productsRouter.get("/:productId", async (request, response, next) => {
 
 productsRouter.put("/:productId", async (request, response, next) => {
     try {
-        const products = await getProducts()
-        const index = products.findIndex(product => product._id === request.params.productId)
-        if (index !== -1) {
-            const oldProduct = products[index]
-            const updatedProduct = { ...oldProduct, ...request.body, updatedAt: new Date() }
-            products[index] = updatedProduct
-
-            await writeProducts(products)
-
-            response.send(updatedProduct)
-
-        } else {
-            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
-        }
-
+        const updatedProduct = await ProductsModel.findByIdAndUpdate(
+            request.params.productId,
+            request.body,
+            { new: true, runValidators: true }
+        )
+        if (!updatedProduct) return next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
+        response.send(updatedProduct)
     } catch (error) {
         next(error)
     }
@@ -78,63 +56,24 @@ productsRouter.put("/:productId", async (request, response, next) => {
 
 productsRouter.delete("/:productId", async (request, response, next) => {
     try {
-        const products = await getProducts()
-        const filteredProducts = products.filter(product => product._id !== request.params.productId)
-
-        if (products.length !== filteredProducts.length) {
-            await writeProducts(filteredProducts)
-            response.status(204).send()
-        } else {
-            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
-        }
+        const deletedProduct = await ProductsModel.findByIdAndDelete(request.params.productId)
+        if (!deletedProduct) return next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
+        response.status(204).send()
     } catch (error) {
         next(error)
     }
 })
 
 
-productsRouter.post("/:productId/upload", multer().single("product"), async (request, response, next) => {
+productsRouter.post("/:productId/reviews", async (request, response, next) => {
     try {
-        if (request.file) {
-            const fileExtension = extname(request.file.originalname)
-            const fileName = request.params.productId + fileExtension
-            await saveProductImage(fileName, request.file.buffer)
-
-            const products = await getProducts()
-            const index = products.findIndex(product => product._id === request.params.productId)
-            if (index !== -1) {
-                const oldProduct = products[index]
-                const updatedProduct = { ...oldProduct, imageUrl: `http://localhost:3000/images/products/${fileName}` }
-                products[index] = updatedProduct
-
-                await writeProducts(products)
-                response.send({ message: "Image uploaded" })
-            } else {
-                next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
-            }
-        }
-        else {
-            next(createHttpError(400, { message: `Upload an image!` }))
-        }
-
-    } catch (error) {
-        next(error)
-    }
-})
-
-
-productsRouter.post("/:productId/reviews", checkReviewSchema, generateBadRequest, async (request, response, next) => {
-    try {
-        const newReview = { _id: uniqid(), ...request.body, productId: request.params.productId, createdAt: new Date(), updatedAt: new Date() }
-        const reviews = await getReviews()
-
-        reviews.push(newReview)
-
-        await writeReviews(reviews)
-
-        response.status(201).send({ _id: newReview._id })
-
-
+        const newReview = new ReviewsModel(request.body)
+        const { _id } = await newReview.save()
+        await ProductsModel.findByIdAndUpdate(
+            request.params.productId,
+            { $push: { reviews: _id } },
+            { new: true, runValidators: true })
+        response.status(201).send({ _id })
     } catch (error) {
         next(error)
     }
@@ -143,14 +82,12 @@ productsRouter.post("/:productId/reviews", checkReviewSchema, generateBadRequest
 
 productsRouter.get("/:productId/reviews", async (request, response, next) => {
     try {
-        const reviews = await getReviews()
-        const validProductId = reviews.some(review => review.productId === request.params.productId)
-        if (validProductId) {
-            const productReviews = reviews.filter(review => review.productId === request.params.productId)
-            response.send(productReviews)
-        } else {
-            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
-        }
+        const foundProduct = await ProductsModel.findById(request.params.productId).populate("reviews")
+        if (!foundProduct) return next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
+
+        const { reviews } = foundProduct
+
+        response.send({ reviews })
     } catch (error) {
         next(error)
     }
@@ -159,19 +96,13 @@ productsRouter.get("/:productId/reviews", async (request, response, next) => {
 
 productsRouter.get("/:productId/reviews/:reviewId", async (request, response, next) => {
     try {
-        const reviews = await getReviews()
-        const validProductId = reviews.some(review => review.productId === request.params.productId)
-        if (validProductId) {
-            const foundReview = reviews.find(review => review._id === request.params.reviewId && review.productId === request.params.productId)
-            if (foundReview) {
-                response.send(foundReview)
-            } else {
-                next(createHttpError(404, { message: `Review with id ${request.params.reviewId} does not exist!` }))
-            }
-        } else {
-            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
-        }
+        const foundProduct = await ProductsModel.findById(request.params.productId).populate("reviews")
+        if (!foundProduct) return next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
 
+        const foundReview = foundProduct.reviews.find(review => review._id.toString() === request.params.reviewId)
+        if (!foundReview) return next(createHttpError(404, { message: `Review with id ${request.params.reviewId} does not exist!` }))
+
+        response.send(foundReview)
     } catch (error) {
         next(error)
     }
@@ -180,26 +111,18 @@ productsRouter.get("/:productId/reviews/:reviewId", async (request, response, ne
 
 productsRouter.put("/:productId/reviews/:reviewId", async (request, response, next) => {
     try {
-        const reviews = await getReviews()
-        const validProductId = reviews.some(review => review.productId === request.params.productId)
-        if (validProductId) {
-            const index = reviews.findIndex(review => review._id === request.params.reviewId && review.productId === request.params.productId)
-            if (index !== -1) {
-                const oldReview = reviews[index]
-                const updatedReview = { ...oldReview, ...request.body, updatedAt: new Date() }
-                reviews[index] = updatedReview
+        const updatedReview = await ReviewsModel.findByIdAndUpdate(
+            request.params.reviewId,
+            request.body,
+            { new: true, runValidators: true }
+        );
 
-                await writeReviews(reviews)
+        if (!updatedReview) return next(createHttpError(404, `Review with id ${request.params.reviewId} not found`));
 
-                response.send(updatedReview)
-            } else {
-                next(createHttpError(404, { message: `Review with id ${request.params.reviewId} does not exist!` }))
+        const foundProduct = await ProductsModel.findById(request.params.productId).populate("reviews")
+        if (!foundProduct) return next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
 
-            }
-
-        } else {
-            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist or has no reviews yet!` }))
-        }
+        response.send(foundProduct)
 
     } catch (error) {
         next(error)
@@ -209,22 +132,17 @@ productsRouter.put("/:productId/reviews/:reviewId", async (request, response, ne
 
 productsRouter.delete("/:productId/reviews/:reviewId", async (request, response, next) => {
     try {
-        const reviews = await getReviews()
-        const validProductId = reviews.some(review => review.productId === request.params.productId)
-        if (validProductId) {
-            const filteredReviews = reviews.filter(review => review._id !== request.params.reviewId)
+        const deletedReview = await ReviewsModel.findByIdAndDelete(request.params.reviewId)
+        if (!deletedReview) return next(404, `Review with id ${request.params.reviewId} does not exist!`)
 
-            if (reviews.length !== filteredReviews.length) {
-                await writeReviews(filteredReviews)
-                response.status(204).send()
-            } else {
-                next(createHttpError(404, { message: `Review with id ${request.params.reviewId} does not exist!` }))
-            }
+        const updatedProduct = await ProductsModel.findByIdAndUpdate(
+            request.params.productId,
+            { $pull: { reviews: request.params.reviewId } },
+            { new: true, runValidators: true })
 
-        } else {
-            next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
-        }
+        if (!updatedProduct) return next(createHttpError(404, { message: `Product with id ${request.params.productId} does not exist!` }))
 
+        response.status(204).send()
     } catch (error) {
         next(error)
     }
